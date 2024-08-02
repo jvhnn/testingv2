0e7f43a772d1c319c514bdac45daaa5638b6f0da
John Marquez <johnmarquez249@gmail.com>
2024-08-01 18:37:55 -0600

    fnbsauifbdnsaujf

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,6 +1,6 @@
 async function fetchQuizData(quizId) {
     
-  const requestURL = `./assets/json/quiz-${2}.json`;
+  const requestURL = `./assets/json/quiz-${quizId}.json`;
 
   try {
     const response = await fetch(requestURL);
